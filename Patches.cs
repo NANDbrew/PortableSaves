616b69acd5c6977b6aa62a26b8005845fbc177a1
NANDbrew <zortman@gmail.com>
2025-02-09 13:26:27 -0600

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using System.IO;
+
+namespace PortableSaves
+{
+    [HarmonyPatch(typeof(SaveSlots))]
+    internal static class PortableSavePatches
+    {
+        [HarmonyPatch("GetBackupPath")]
+        [HarmonyPostfix]
+        public static void GetBackupPathPatch(int slot, int backupIndex, ref string __result)
+        {
+            __result = GetPath(slot, backupIndex);
+            Debug.Log("Overrode path with " + __result);
+        }
+
+        [HarmonyPatch("GetCurrentSavePath")]
+        [HarmonyPostfix]
+        public static void GetCurrentSavePathPatch(int ___currentSlot, ref string __result)
+        {
+            __result = GetPath(___currentSlot, -1);
+            Debug.Log("Overrode path with " + __result);
+        }
+
+        [HarmonyPatch("GetSlotSavePath")]
+        [HarmonyPostfix]
+        public static void GetSlotSavePathPatch(int slot, ref string __result)
+        {
+            __result = GetPath(slot, -1);
+            Debug.Log("Overrode path with " + __result);
+        }
+        private static string GetPath(int slot, int backupIndex)
+        {
+            string basePath = Application.dataPath + "/Saves";
+            if (!Directory.Exists(basePath)) Directory.CreateDirectory(basePath);
+            string backup = backupIndex != -1 ? "_backup" + backupIndex : "";
+            string path = basePath + "/slot" + slot.ToString() + backup + ".save";
+            return path;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace PortableSaves
+{
+    [BepInPlugin(PLUGIN_ID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_ID = "com.nandbrew.PortableSaves";
+        public const string PLUGIN_NAME = "Portable Saves";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        //--settings--
+        //internal ConfigEntry<bool> someSetting;
+
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
+
+            //someSetting = Config.Bind("Settings", "Some setting", false);
+        }
+    }
+}
